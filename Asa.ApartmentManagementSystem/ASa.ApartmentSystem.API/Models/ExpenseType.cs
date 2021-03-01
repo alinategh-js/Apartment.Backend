87a61d7a4b7a95280213c7039e64d07ac08a4be4
Ali <a.nategh76@gmail.com>
2021-03-01 10:09:33 +0330

    fixed conflicts of duplicated api folders

---
+++ Asa.ApartmentManagementSystem/ASa.ApartmentSystem.API/Models/ExpenseType.cs
@@ -0,0 +1,15 @@
+﻿using ASa.ApartmentManagement.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asa.ApartmentSystem.API.Models
+{
+    public class ExpenseType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public FormulaType Formula { get; set; }
+    }
+}
---
+++ Asa.ApartmentManagementSystem/ASa.ApartmentSystem.API/Models/PeopleRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asa.ApartmentSystem.API.Models
+{
+    public class PeopleRequest
+    {
+        public int Page { get; set; }
+        public int Size { get; set; }
+        public int IsOwner { get; set; }
+    }
+}
